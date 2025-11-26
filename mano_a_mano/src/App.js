cab03d28f24f55fda5e85368a011e02b4199ab2d
Lizeth De Jesus <lizethdejesusgonz@gmail.com>
2025-11-26 00:53:57 -0600

    complementacion al Login

--- mano_a_mano/src/App.js
+++ mano_a_mano/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect} from 'react'
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+import {supabase} from './supabase/client';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        navigate('/login') //si el usuario no esta autenticado se envia a login
+      }else {
+        navigate('/') //si el usuario ya esta autenticado envialo a home 
+      }
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </div>
   );
 }
